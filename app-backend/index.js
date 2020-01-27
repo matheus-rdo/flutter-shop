ee75ced698ab9aad13ca009bd4323ddfe47caaa7
Matheus.Raymundo <Matheus.Raymundo@senior.com.br>
2020-01-27 16:47:18 -0300

    Add de latência intencional

--- app-backend/index.js
+++ app-backend/index.js
@@ -7,6 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors())
+app.use((req, res, next) => setTimeout(next, 1500));
 
 //Modules API's
 app.use('/products', productsAPI)
